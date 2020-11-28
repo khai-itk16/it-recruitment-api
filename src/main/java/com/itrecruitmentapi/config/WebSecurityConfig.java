ef4c9bbc5a59363f323ab23611fff540ef06703b
khai_itk16 <minhkhaia12016@gmail.com>
2020-11-28 09:49:20 +0700

    config to deploy

--- src/main/java/com/itrecruitmentapi/config/WebSecurityConfig.java
+++ src/main/java/com/itrecruitmentapi/config/WebSecurityConfig.java
@@ -1,6 +1,12 @@
 package com.itrecruitmentapi.config;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
 import com.itrecruitmentapi.config.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,12 +24,44 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
     @Qualifier("userDetailsServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private GoogleCredential googleCredential;
+
+    @Bean
+    public Drive getService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Drive.Builder(HTTP_TRANSPORT,
+                JacksonFactory.getDefaultInstance(), googleCredential)
+                .build();
+    }
+
+    @Bean
+    public GoogleCredential googleCredential() throws GeneralSecurityException, IOException {
+        Collection<String> elenco = new ArrayList<String>();
+        elenco.add("https://www.googleapis.com/auth/drive");
+        HttpTransport httpTransport = new NetHttpTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+        return new GoogleCredential.Builder()
+                .setTransport(httpTransport)
+                .setJsonFactory(jsonFactory)
+                .setServiceAccountId("datn-image-storage@angelic-cat-276507.iam.gserviceaccount.com")
+                .setServiceAccountScopes(elenco)
+                .setServiceAccountPrivateKeyFromP12File(new File("path/to/google-service-key.p12"))
+                .build();
+    }
 
     @Override
     @Bean
